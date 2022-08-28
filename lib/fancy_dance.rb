eaa73832d6a7adc4451c715e26ba636ffc963adb
Allan <kipropsallan@gmail.com>
2022-08-28 11:12:32 +0300

    commit tree c16ac8f8361dd150303acf9b2d0ab42a2e5c16cd

---
+++ lib/fancy_dance.rb
@@ -0,0 +1,28 @@
+module FancyDance
+    module InstanceMethods
+  
+      def twirl
+        "I'm twirling!"
+      end
+  
+      def jump
+        "Look how high I'm jumping!"
+      end
+  
+      def pirouette
+
+      end
+  
+      def take_a_bow
+
+      end
+    end
+  
+    module ClassMethods
+  
+      def metadata
+
+      end
+
+    end
+  end
